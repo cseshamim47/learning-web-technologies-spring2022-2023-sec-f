aa656f8f041a1aeae8d2aa543399750a52a97cf7
Md Shamim Ahmed <cseshamim47@gmail.com>
2023-02-19 06:55:09 +0600

    updating few files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learning-web-technologies-spring2022-2023-sec-f
+
+20-44242-3
--- test.php
+++ test.php
@@ -1,15 +1,51 @@
-<?php
+<?php 
 
+$student = ['id'=>1, 'name'=>'alamin', 'cgpa'=>3.5];
 
+$name = "shamim";
+// echo $student;
 
-for($i = 'A'; $i >= 'C'; $i--)
-{
-    // for($j = 1; $j <= $i; $j++)
-    // {
-    //     echo $j . " ";
-    // }
-    // echo "<br>";
-    echo $i;
+$students = [
+    's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>3.5],
+    's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>3.4],
+    's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3],
+    's4'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3],
+    's5'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3],
+    's6'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3],
+    's7'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3]
+];
+
+echo "<table border=1>
+<tr>
+    <td>ID</td>
+    <td>NAME</td>
+    <td>CGPA</td>
+</tr>";
+
+foreach($students as $s){
+    echo    "<tr>
+                <td>{$s['id']}</td>
+                <td>{$s['name']}</td>
+                <td>{$s['cgpa']}</td>
+            </tr>";
 }
+echo "</table>";
 
 ?>
+
+<table border="1">
+        <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>CGPA</td>
+        </tr>
+        <?php foreach($students as $s){ ?>
+        <tr>
+            <td><?php echo $s['id']?></td>
+            <td><?=$s['name']?></td>
+            <td><?=$s['cgpa']?></td>
+        </tr>
+
+        <?php } ?>
+</table>
+
