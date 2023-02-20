63965d7f013416536dadc6d06fd93c68724f5677
Md Shamim Ahmed <cseshamim47@gmail.com>
2023-02-20 15:17:17 +0600

    20 feb

---
+++ LAB-5.2 [PHP] [Session Tracking]/publicHome.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <table border="1" width=100%>
+        <tr height="100px">
+            <th width=20%>
+                
+
+            </th>
+            <th width=55%></th>
+            <th>
+                <a href="publicHome.php">Home</a> |
+                <a href="login.php">Login</a> |
+                <a href="login.php">Registration</a>
+                
+            </th>
+        </tr>
+
+        <tr height="200px">
+            
+        </tr>
+
+        <tr height="80px">
+            
+        </tr>
+    </table>
+</body>
+</html>
